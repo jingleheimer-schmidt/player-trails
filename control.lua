93ca209b45237f00f409cf0467d15b3ef0bedc07
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2022-12-21 10:26:11 -0800

    version 0.0.1

---
+++ control.lua
@@ -0,0 +1,197 @@
+
+--[[
+Player Trails control script © 2022 by asher_sky is licensed under Attribution-NonCommercial-ShareAlike 4.0 International. See LICENSE.txt for additional information
+--]]
+
+local speeds = {
+  veryslow = 0.010,
+  slow = 0.025,
+  default = 0.050,
+  fast = 0.100,
+  veryfast = 0.200,
+}
+
+local palette = {
+  light = {amplitude = 15, center = 240},           -- light
+  pastel = {amplitude = 55, center = 200},          -- pastel <3
+  default = {amplitude = 127.5, center = 127.5},    -- default (nyan)
+  vibrant = {amplitude = 50, center = 100},         -- muted
+  deep = {amplitude = 25, center = 50},             -- dark
+}
+
+local sin = math.sin
+local pi_div_3 = math.pi / 3
+
+function make_rainbow(rainbow, game_tick, player_settings)
+  local index = rainbow.player_index
+  local created_tick = rainbow.tick
+  -- local player_settings = settings[index]
+  local frequency = speeds[player_settings["player-trail-speed"]]
+  if player_settings["player-trail-sync"] == true then
+    created_tick = index
+  end
+  local modifier = (game_tick)+(index*created_tick)
+  local palette_key = player_settings["player-trail-palette"]
+  local amplitude = palette[palette_key].amplitude
+  local center = palette[palette_key].center
+  return {
+    r = sin(frequency*(modifier)+(0*pi_div_3))*amplitude+center,
+    g = sin(frequency*(modifier)+(2*pi_div_3))*amplitude+center,
+    b = sin(frequency*(modifier)+(4*pi_div_3))*amplitude+center,
+    a = 255,
+  }
+end
+
+local function initialize_settings(index)
+  if not global.settings then
+    global.settings = {}
+  end
+  local player_settings = settings.get_player_settings(index)
+  global.settings[index] = {}
+  global.settings[index]["player-trail-glow"] = player_settings["player-trail-glow"].value
+  global.settings[index]["player-trail-color"] = player_settings["player-trail-color"].value
+  global.settings[index]["player-trail-animate"] = player_settings["player-trail-animate"].value
+  global.settings[index]["player-trail-length"] = player_settings["player-trail-length"].value
+  global.settings[index]["player-trail-scale"] = player_settings["player-trail-scale"].value
+  global.settings[index]["player-trail-speed"] = player_settings["player-trail-speed"].value
+  global.settings[index]["player-trail-sync"] = player_settings["player-trail-sync"].value
+  global.settings[index]["player-trail-palette"] = player_settings["player-trail-palette"].value
+  global.settings[index]["player-trail-taper"] = player_settings["player-trail-taper"].value
+  global.settings[index]["player-trail-type"] = player_settings["player-trail-type"].value
+end
+
+script.on_event(defines.events.on_runtime_mod_setting_changed, function(event)
+  initialize_settings(event.player_index)
+end)
+
+script.on_configuration_changed(function(event)
+  for each, player in pairs(game.players) do
+    initialize_settings(player.index)
+  end
+end)
+
+script.on_event(defines.events.on_player_joined_game, function(event)
+  initialize_settings(event.player_index)
+end)
+
+script.on_event(defines.events.on_player_changed_position, function(event)
+  local player_index = event.player_index
+  if not (global.settings and global.settings[player_index]) then
+    initialize_settings(player_index)
+  end
+  local player_settings = global.settings[player_index]
+  local sprite = player_settings["player-trail-color"]
+  local light = player_settings["player-trail-glow"]
+  local player = {}
+  if sprite or light then
+    player = game.get_player(player_index)
+    if not (player.controller_type == defines.controllers.character) then
+      return
+    end
+  else
+    return
+  end
+  local event_tick = event.tick
+  local length = tonumber(player_settings["player-trail-length"])
+  local scale = tonumber(player_settings["player-trail-scale"])
+  if sprite then
+    sprite = rendering.draw_sprite{
+      sprite = "player-trail",
+      target = player.position,
+      surface = player.surface,
+      x_scale = scale,
+      y_scale = scale,
+      render_layer = "radius-visualization",
+      time_to_live = length,
+    }
+    if not global.sprites then
+      global.sprites = {}
+    end
+    local sprite_data = {
+      sprite = sprite,
+      tick_to_die = event_tick + length,
+      size = (scale + length) * 4,
+      tick = event_tick,
+      player_index = player_index,
+    }
+    global.sprites[sprite] = sprite_data
+    local rainbow_color = player.color
+    if player_settings["player-trail-type"] == "rainbow" then
+      rainbow_color = make_rainbow(sprite_data, event_tick, player_settings)
+    end
+    rendering.set_color(sprite, rainbow_color)
+  end
+  if light then
+    light = rendering.draw_light{
+      sprite = "player-trail",
+      target = player.position,
+      surface = player.surface,
+      intensity = .175,
+      scale = scale * 2,
+      render_layer = "light-effect",
+      time_to_live = length,
+    }
+    if not global.lights then
+      global.lights = {}
+    end
+    local light_data = {
+      light = light,
+      tick_to_die = event_tick + length,
+      size = (scale + length) * 4,
+      tick = event_tick,
+      player_index = player_index,
+    }
+    global.lights[light] = light_data
+    local rainbow_color = player.color
+    if player_settings["player-trail-type"] == "rainbow" then
+      rainbow_color = make_rainbow(light_data, event_tick, player_settings)
+    end
+    rendering.set_color(light, rainbow_color)
+  end
+end)
+
+script.on_event(defines.events.on_tick, function(event)
+  local render_ids = rendering.get_all_ids("player-trails")
+  if not render_ids then
+    return
+  end
+  local settings = global.settings
+  local game_tick = event.tick
+  for _, id in pairs(render_ids) do
+    local rainbow = global.sprites[id] or global.lights[id]
+    if rainbow then
+      if rainbow.tick_to_die <= game_tick then
+          global.sprites[id] = nil
+          global.lights[id] = nil
+      else
+        local player_settings = settings[rainbow.player_index]
+        if not player_settings["player-trail-animate"] then
+          return
+        end
+        if not (player_settings["player-trail-type"] == "rainbow") then
+          return
+        end
+        local sprite = rainbow.sprite
+        local light = rainbow.light
+        local rainbow_color = make_rainbow(rainbow, game_tick, player_settings)
+        local size = rainbow.size
+        if sprite then
+          if player_settings["player-trail-taper"] then
+            local scale = rendering.get_x_scale(sprite)
+            scale = scale - scale / size
+            rendering.set_x_scale(sprite, scale)
+            rendering.set_y_scale(sprite, scale)
+          end
+          rendering.set_color(sprite, rainbow_color)
+        elseif light then
+          if player_settings["player-trail-taper"] then
+            local scale = rendering.get_scale(light)
+            scale = scale - scale / size
+            rendering.set_scale(light, scale)
+          end
+          rendering.set_color(light, rainbow_color)
+        end
+      end
+    end
+  end
+end)
---
+++ settings.lua
@@ -0,0 +1,148 @@
+local rainbowColorSetting = {
+  type = "bool-setting",
+  name = "player-trail-color",
+  setting_type = "runtime-per-user",
+  order = "a",
+  default_value = true
+}
+
+local rainbowGlowSetting = {
+  type = "bool-setting",
+  name = "player-trail-glow",
+  setting_type = "runtime-per-user",
+  order = "b",
+  default_value = true
+}
+
+local rainbowAnimateSetting = {
+  type = "bool-setting",
+  name = "player-trail-animate",
+  setting_type = "runtime-per-user",
+  order = "c",
+  default_value = true
+}
+
+local rainbowTaperSetting = {
+  type = "bool-setting",
+  name = "player-trail-taper",
+  setting_type = "runtime-per-user",
+  order = "d",
+  default_value = true
+}
+
+local rainbowSyncSetting = {
+  type = "bool-setting",
+  name = "player-trail-sync",
+  setting_type = "runtime-per-user",
+  order = "e",
+  default_value = false
+}
+
+local rainbowScaleSetting = {
+  type = "string-setting",
+  name = "player-trail-scale",
+  setting_type = "runtime-per-user",
+  order = "f",
+  default_value = "5",
+  allowed_values = {
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "8",
+    "11",
+    "20",
+  }
+}
+
+local rainbowLengthSetting = {
+  type = "string-setting",
+  name = "player-trail-length",
+  setting_type = "runtime-per-user",
+  order = "g",
+  default_value = "120",
+  allowed_values = {
+    "15",
+    "30",
+    "60",
+    "90",
+    "120",
+    "180",
+    "210",
+    "300",
+    "600"
+  }
+}
+
+local rainbowColorTypeSetting = {
+  type = "string-setting",
+  name = "player-trail-type",
+  setting_type = "runtime-per-user",
+  order = "h",
+  default_value = "rainbow",
+  allowed_values = {
+    "player",
+    "rainbow"
+  }
+}
+
+local rainbowPaletteSetting = {
+  type = "string-setting",
+  name = "player-trail-palette",
+  setting_type = "runtime-per-user",
+  order = "i",
+  default_value = "default",
+  allowed_values = {
+    "light",
+    "pastel",
+    "default",
+    "vibrant",
+    "deep"
+  }
+}
+
+local rainbowSpeedSetting = {
+  type = "string-setting",
+  name = "player-trail-speed",
+  setting_type = "runtime-per-user",
+  order = "j",
+  default_value = "default",
+  allowed_values = {
+    "veryslow",
+    "slow",
+    "default",
+    "fast",
+    "veryfast"
+  }
+}
+
+-- local rainbowTrailsBalance = {
+--   type = "string-setting",
+--   name = "player-trail-balance",
+--   setting_type = "runtime-global",
+--   order = "k",
+--   default_value = "super-pretty",
+--   allowed_values = {
+--     -- "super-performance",
+--     "performance",
+--     "balanced",
+--     "pretty",
+--     "super-pretty"
+--   }
+-- }
+
+data:extend({
+  rainbowColorSetting,
+  rainbowGlowSetting,
+  rainbowAnimateSetting,
+  rainbowTaperSetting,
+  rainbowSyncSetting,
+  rainbowScaleSetting,
+  rainbowLengthSetting,
+  rainbowColorTypeSetting,
+  rainbowPaletteSetting,
+  rainbowSpeedSetting,
+  -- rainbowTrailsBalance
+})
